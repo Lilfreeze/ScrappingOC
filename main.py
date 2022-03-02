f0a07ed34b9ffe7f2a7c7637ad021e7bdd6142c2
Lilfreeze <kientzel77@hotmail.com>
2022-03-02 21:50:04 +0100

    Récupération des infos dans un tableau

--- main.py
+++ main.py
@@ -5,3 +5,39 @@ url = "http://books.toscrape.com/catalogue/a-light-in-the-attic_1000/index.html"
 page = requests.get(url)
 soup = BeautifulSoup(page.content, 'html.parser')
 
+#Initialisation du tableau
+tableau = []
+
+#URL de la page
+tableau.append(url)
+
+#Tire du livre
+titre = soup.find("li", class_="active").string
+tableau.append(titre)
+
+#Product Description
+description = soup.find("div", class_="sub-header")
+tableau.append(description.next_element.next_element.next_element.next_element.next_element.next_element.string)
+
+#Product information
+table = soup.find_all("td")
+tableau.append(table[0].string)
+tableau.append(table[2].string)
+tableau.append(table[3].string)
+tableau.append(table[5].string)
+
+#Category
+category = soup.find("ul", class_="breadcrumb")
+tableau.append(category.next_element.next_element.next_element.next_element.next_element.next_element.next_element.next_element.next_element.next_element.next_element.next_element.next_element.next_element.next_element.next_element.string)
+
+#image URL
+imgURL = soup.find("img")
+imgURL = "http://books.toscrape.com/" + imgURL["src"]
+tableau.append(imgURL)
+
+#Stars
+stars = soup.find("p", class_="star-rating")
+tableau.append(stars["class"][1])
+
+print(tableau)
+
